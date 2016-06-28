9363bdd511a8e6173b31780551ecbe65d08dd6a6
Casie Lynch <casie@primeacademy.io>
2016-06-28 15:56:46 -0400

    initial commit

---
+++ public/javascripts/app.js
@@ -0,0 +1,41 @@
+/**
+ * Created by casie on 6/28/16.
+ */
+//Ajax
+//REST - standardization of communication with server
+
+//Get - pulls info from server
+//Post - sends info to server
+//Put - modifies existing file in server
+//Delete - deletes file from server
+
+//Connection between client and server
+
+//Ajax - asynchronous javascript and xml
+
+//Request -
+//Response -
+
+//
+var fortuneCookies = null;
+
+$(document).ready(function(){
+    $('#button').on('click',function(){
+
+        $('.here').empty();
+
+        $.get('/javascripts/data.json', function(data) {
+            fortuneCookies = data;
+
+            var i = Math.floor((Math.random() * fortuneCookies.firsts.length-1) + 1);
+            var first = fortuneCookies.firsts[i].first;
+            var i = Math.floor((Math.random() * fortuneCookies.seconds.length-1) + 1);
+            var second = fortuneCookies.seconds[i].second;
+            var i = Math.floor((Math.random() * fortuneCookies.thirds.length-1) + 1);
+            var third = fortuneCookies.thirds[i].third;
+
+            console.log("It worked");
+            $(".here").append("<div><p>" + first + " " + second + " " + third + "</p></div>").hide().fadeIn("slow");
+        });
+    });
+});
